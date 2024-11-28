ab6d1b3dedda503586eab805c0ada387c037af8b
Igor B <bazhew@mail.ru>
2024-11-28 15:06:35 +0500

    Completed 5_13

--- c/os/5_13.c
+++ c/os/5_13.c
@@ -5,82 +5,86 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-enum { cmd_max_len = 64, list_max_len = 64 };
-
-/*
-int get_command_list(char **dest, int argc, char **argv)
-{
-	int i, cur_pos;
-	char cmd[cmd_max_len];
-
-	for(i = 1, cur_pos = 0; i < argc; i++) {
-		if(*(argv[i]) == ';')
-			continue;
-		dest[cur_pos] = argv[i];
-		cur_pos++;
-	}
-
-	dest[cur_pos] = NULL;
-	return cur_pos;
-}
-*/
+enum { name_max_len = 64 };
 
 struct proc_info {
 	char *prog_name;
-	char **params;
+	char **args;
 	int pid;
 };
 
-void execute_command(struct proc_info *prog)
+void execute_command(struct proc_info *pc)
 {
-	execvp(prog->prog_name, prog->params);
-	perror(prog->prog_name);
+	execvp(pc->prog_name, pc->args);
+	perror(pc->prog_name);
 	exit(1);
 }
 
-char *get_command(char **argv)
+int get_command(char **argv, char **cmd)
 {
-	char *cmd[cmd_max_len];
 	int i;
 
-	for(i = 0; !strcmp(argv[i], NULL) && !strcmp(argv[i], ";;"); i++) {
-		strcpy(cmd[i], argv[i]);
+	for(i = 0; argv[i] != NULL && strcmp(argv[i], ";;") != 0; i++) {
+		cmd[i] = argv[i];
 	}
 	cmd[i] = NULL;
 	
-	return cmd;
+	return i;
 }
 
-struct proc_info *get_command_list(int argc, char **argv)
+struct proc_info *get_prog_by_pid(int wr, struct proc_info **pptr)
 {
-	char **cmd;
-	struct proc_info *first = NULL, *tmp = NULL;
-	int i;
-
-	cmd = get_command(argv);
-
-	for(i = 1; i < argc; i++) {
-		if(strcmp(argv[i], ";;"))
-			continue;
-		tmp = malloc(sizeof(struct proc_info));
-		
+	while(*pptr != NULL) {
+		if((*pptr)->pid == wr) {
+			return *pptr;
+		}
+		pptr++;
 	}
+
+	return NULL;
+}
+
+void release_memory(struct proc_info **pptr)
+{
+	struct proc_info **tmp = pptr;
+
+	while(*tmp != NULL) {
+		free(*tmp);
+		tmp++;
+	}
+	free(pptr);
 }
 
 int main(int argc, char **argv)
 {
 	int wr, status, pid;
+	int offset;
 	int i;
-	struct proc_info b;
+	struct proc_info **pptr;
+	struct proc_info **ptr_pc;
+	struct proc_info *pc;
+	char *cmd[name_max_len];
 
 	if(argc < 2) {
 		fprintf(stderr, "Provide at least one prog to run\n");
 		exit(-1);
 	}
 
-	for(i = 1; i < argc;) {
+	ptr_pc = malloc((argc / 2) * sizeof(struct proc_info *));
+	pptr = ptr_pc;
 
-		
+	for(i = 1; i < argc;) {
+		if((offset = get_command(&(argv[i]), cmd)) != 0) {
+			i += offset;
+			pc = malloc(sizeof(struct proc_info));
+			pc->prog_name = cmd[0];
+			pc->args = &(cmd[0]);
+			*pptr = pc;
+			pptr++;
+		} else {
+			i++;
+			continue;
+		}
 
 		pid = fork();
 		if(pid == -1) {
@@ -88,17 +92,24 @@ int main(int argc, char **argv)
 			return 1;
 		}
 		if(pid == 0) {
-			execute_command();
+			execute_command(pc);
 		}
+		pc->pid = pid;
 	}
+
+	*pptr = NULL;
+	pptr = ptr_pc;
 
 	while((wr = wait(&status)) != -1) {
-		printf("Process with pid=%d finished.\n", wr);
 		if(WIFEXITED(status)) {
-			printf("It has exited with code=%d\n",
-				WEXITSTATUS(status));
-		}	
-	}
+			pc = get_prog_by_pid(wr, pptr);
+			printf(
+				"Program \"%s\" with pid=%d successfully completed.\n",
+				pc->prog_name, wr
+				);
+		}
+	}	
 
+	release_memory(ptr_pc);
 	exit(0);
 }
